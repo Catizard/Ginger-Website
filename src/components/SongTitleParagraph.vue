9afdcac611af2c54220ab44e9ca51d1cf2c98656
Catizard <Salieri_XY@163.com>
2026-06-23 21:26:16 +0800

    chore: align the missing icon

--- src/components/SongTitleParagraph.vue
+++ src/components/SongTitleParagraph.vue
@@ -1,10 +1,10 @@
 <template>
   <n-flex vertical :size="4">
-    <n-flex>
-      <n-icon v-if="lost" :component="WarningOutline" color="red" />
+    <n-flex inline style="gap: 8px 6px; align-items: center;">
+      <n-icon class="lined-icon" size="18" v-if="lost" :component="WarningOutline" color="red" />
       <strong style="font-size: 1.1em;">{{ data.title }}</strong>
     </n-flex>
-    <n-text v-if="data.artist != ''" depth="3" style="font-size: 0.9em;">{{ data.artist }}</n-text>
+    <n-text v-if="data.artist != ''" depth="3" style="font-size: 0.9em; line-height: 18px;">{{ data.artist }}</n-text>
   </n-flex>
 </template>
 
